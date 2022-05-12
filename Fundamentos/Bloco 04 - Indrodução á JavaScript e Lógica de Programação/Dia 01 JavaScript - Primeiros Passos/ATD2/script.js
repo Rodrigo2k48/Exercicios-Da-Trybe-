94a312ec692f214606e6cb641b4cf4d209cef22e
Rodrigo Gonçalves <Rodrigoga701@gmail.com>
2022-05-12 15:59:22 -0300

    Atividade 2 Concluida (usando operadores no codigo tanto operações printando tanto fazendo elas no codigo)

---
+++ Fundamentos/Bloco 04 - Indrodução á JavaScript e Lógica de Programação/Dia 01 JavaScript - Primeiros Passos/ATD2/script.js
@@ -0,0 +1,16 @@
+/* Operadores Aritimeticos no Js
+Usando no codigo e usando junto 
+para printar no console */
+
+const base = 5;
+
+let height = 8;
+
+const area = 40;
+
+const perimeter = 5 + 8 + 40;
+
+console.log(perimeter)
+
+
+//console.log(base*height)
